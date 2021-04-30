1095709714b817f944809414d443fae2cde5875d
AaronBui-gif <56668881+AaronBui-gif@users.noreply.github.com>
2021-05-01 01:59:51 +0700

    Update VideoStore.cpp

--- CPP Files/VideoStore.cpp
+++ CPP Files/VideoStore.cpp
@@ -8,28 +8,74 @@
 #include <string>
 
 using namespace std;
-int main(int argc, char* argv[]){
 
+/*** MAIN FUNCTION ***/
+int main(int argc, char* argv[]){
+    printMenu();
+    getOption();
 }
 
+/*** FUNCTION to print out menu ***/
 void printMenu(){
-    string input;
-    cout << "Select an option: " << endl;
-    cout << "1. Item" << endl;
-    cout << "2. Account" << endl;
-    cin >> input;
-    if (input == 1){
-        cout << "************************************************************************" << endl;
-        cout << "1. Add an item" << endl;
-        cout << "2. Update an item" << endl;
-        cout << "3. Delete an item" << endl;
-        cout << "4. Rent an item" << endl;
-    }
-    else if (input == 2) {
-        cout << "************************************************************************" << endl;
-        cout << "1. Add an account" << endl;
-        cout << "2. Update an account" << endl;
-        cout << "3. Delete an account" << endl;
-    }
+    cout << endl << "******************************" << endl;
+         << "Welcome to Genie’s video store" << endl
+         << "Enter an option below." << endl
+         << "1.Add a new item, update or delete an existing item" << endl
+         << "2.Add new customer or update an existing customer" << endl
+         << "3.Promote an existing customer" << endl
+         << "4.Rent an item" << endl
+         << "5.Return an item" << endl
+         << "6.Display all items" << endl
+         << "7.Display out-of-stock items" << endl
+         << "8.Display all customers" << endl
+         << "9.Display group of customers" << endl
+         << "10.Search items or customers" << endl
+         << "Exit." << endl
+}
 
+/*** FUNCTION to get input option from the user ***/
+void getOption() {
+    // Declare variables
+    string input;
+    do {
+        cin << input;
+        if (input == 1) {
+
+        }
+        else if (input == 2) {
+
+        }
+        else if (input == 3) {
+
+        }
+        else if (input == 4) {
+
+        }
+        else if (input == 5) {
+
+        }
+        else if (input == 6) {
+
+        }
+        else if (input == 7) {
+
+        }
+        else if (input == 8) {
+
+        }
+        else if (input == 9) {
+
+        }
+        else if (input == 10) {
+
+        }
+        else if (input == "Exit") {
+            cout << "Closing Genie's video store app" << endl;
+            cout << "Good Bye" << endl;
+            cout << "******************************" << endl;
+        }
+        else {
+            cout << "User's input is invalid" << endl;
+        }
+    } while (input >= 1 && input <= 10);
 }
